ccdfb93e4b64e3f24173c8fad592b3cd29659a1b
fredddyrp83 <frp.plata@gmail.com>
2022-05-26 20:42:58 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./madboy.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="ma" />
+        <p>This is Ramon new App lets hack back to work with this</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
