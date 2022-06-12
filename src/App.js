8682cc3c0fd6b3ee3b252ef075484d525cb2e13e
fredddyrp83 <frp.plata@gmail.com>
2022-06-12 13:26:33 -0500

    add the images files

--- src/App.js
+++ src/App.js
@@ -5,27 +5,27 @@ function App() {
     {
       id: 1,
       title: "Pijamas",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      imageUrl: "images/uno.jpg",
     },
     {
       id: 2,
       title: "Batas",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      imageUrl: "images/batas1.jpg",
     },
     {
       id: 3,
-      title: "Cojies",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      title: "Pantuflas",
+      imageUrl: "images/pantuflas.jpg",
     },
     {
       id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      title: "Cojines",
+      imageUrl: "images/cojines.jpg",
     },
     {
       id: 5,
       title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      imageUrl: "images/uno.jpg",
     },
   ];
   return (
--- src/components/category/catergory.jsx
+++ src/components/category/catergory.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 import "./categories.scss";
 
-const CategoryItem = ({category}) => {
-
-const {imageUrl, title} = category
+const CategoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
   return (
     <>
-     
-        <div className="category-container" >
-          <div
-            className="background-image"
-            style={{
-              backgroundImage: `url(${imageUrl})`,
-            }}
-          />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
+      <div className="category-container">
+        <div
+          className="background-image"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+          }}
+        />
+        <div className="category-body-container">
+          <h2>{title}</h2>
+          <p>Shop Now</p>
         </div>
-      
+      </div>
     </>
   );
 };
